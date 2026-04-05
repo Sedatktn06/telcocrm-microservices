8a7d947585fe491d5b12e62367dc9518983587fc
Sedat Kotan <sedatkotan06@gmail.com>
2026-04-05 22:04:44 +0300

    Unit Of Work Implementation

---
+++ building-blocks/core/Persistence/Contexts/EfDbContextBase.cs
@@ -0,0 +1,94 @@
+﻿using CoreAbstraction.ContextExecutions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Data;
+
+namespace Persistence.Contexts;
+
+public class EfDbContextBase : DbContext, IUnitOfWork
+{
+    IDbContextTransaction _dbContextTransaction;
+
+    protected EfDbContextBase(DbContextOptions options) : base(options)
+    {
+        System.Diagnostics.Debug.WriteLine($"{GetType().Name}::ctor");
+    }
+
+    public async Task BeginTransactionAsync(IsolationLevel isolationLevel, CancellationToken cancellationToken = default)
+    {
+        _dbContextTransaction ??= await Database.BeginTransactionAsync(isolationLevel,cancellationToken);
+    }
+
+    public async Task CommitTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await SaveChangesAsync(cancellationToken);
+            await _dbContextTransaction.CommitAsync(cancellationToken);
+        }
+        catch (Exception)
+        {
+            await RollbackTransactionAsync(cancellationToken);
+            throw;
+        }
+        finally
+        {
+            await _dbContextTransaction.DisposeAsync();
+            _dbContextTransaction = null;
+        }
+    }
+
+    public Task ExecuteTransactionalAsync(Func<Task> action, CancellationToken cancellationToken = default)
+    {
+        var strategy = Database.CreateExecutionStrategy();
+
+        return strategy.ExecuteAsync(async () =>
+        {
+            await BeginTransactionAsync(IsolationLevel.ReadCommitted, cancellationToken);
+            try
+            {
+                await action();
+                await CommitTransactionAsync(cancellationToken);
+            }
+            catch (Exception)
+            {
+                await RollbackTransactionAsync(cancellationToken);
+                throw;
+            }
+        });
+    }
+
+    public Task<T> ExecuteTransactionalAsync<T>(Func<Task<T>> action, CancellationToken cancellationToken = default)
+    {
+        var strategy = Database.CreateExecutionStrategy();
+
+        return strategy.ExecuteAsync(async () =>
+        {
+            await BeginTransactionAsync(IsolationLevel.ReadCommitted, cancellationToken);
+            try
+            {
+                var result = await action();
+                await CommitTransactionAsync(cancellationToken);
+                return result;
+            }
+            catch (Exception)
+            {
+                await RollbackTransactionAsync(cancellationToken);
+                throw;
+            }
+        });
+    }
+
+    public async Task RollbackTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await _dbContextTransaction.RollbackAsync(cancellationToken);
+        }
+        finally 
+        { 
+            _dbContextTransaction.Dispose();
+            _dbContextTransaction = null;
+        }
+    }
+}
